099ed6c58038c41afa7b329582228b3dab898698
Alex <alex.leppakoski@hotmail.com>
2019-12-02 16:49:20 +0200

    Added z component, dot and subtract methods

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import lib.vector as vec
 import lib.polygon as pol
 
-a = vec.Vector(3, 4)
-a = a.scale(10)
-a.x = 5
-print('toimii: ' + str(a.x) + ' ' + str(a.y))
+a = vec.Vector(3, 4, 0)
+b = vec.Vector(2, 8, 0)
+
+sub = a.subtract(b)
+print('subtract: ' + str(sub.x) + ' ' + str(sub.y))
+
+dot = a.dot(b)
+print('dot ' + str(dot))
+
+scale = a.scale(10)
+print('scale ' + str(scale.x) + ' ' + str(scale.y))
--- lib/vector.py
+++ lib/vector.py
@@ -1,23 +1,24 @@
 
 class Vector:
-    def __init__(self, x: float, y: float):
+    def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.y = y
+        self.z = z
 
     def add(self, other: 'Vector') -> 'Vector':
         return self
 
     def subtract(self, other: 'Vector') -> 'Vector':
-        return self
+        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
 
     def cross(self, other: 'Vector') -> 'Vector':
         return self
 
-    def dot(self, other: 'Vector') -> 'Vector':
-        return self
+    def dot(self, other: 'Vector') -> float:
+        return self.x * other.x + self.y * other.y + self.z * other.z
 
     def scale(self, scalar: float) -> 'Vector':
-        return Vector(self.x * scalar, self.y * scalar)
+        return Vector(self.x * scalar, self.y * scalar, self.z * scalar)
 
     def normalize(self) -> 'Vector':
         return self
