3f9239502473e5159f6494c275e17bc42348319e
Alex <alex.leppakoski@hotmail.com>
2019-12-05 22:43:20 +0200

    Added more polygons, ignoring identical collisions

--- app.py
+++ app.py
@@ -15,9 +15,17 @@ polygons[0].angle = 4
 polygons.append(pol.Polygon(1, 2, 1, 4))
 polygons[1].position = vec.Vector(8,5,0)
 
-polygons.append(pol.Polygon(1, 2, 1, 3))
+polygons.append(pol.Polygon(1, 2, 1, 6))
 polygons[2].position = vec.Vector(4,2,0)
 polygons[2].velocity = vec.Vector(20,20,0)
+
+polygons.append(pol.Polygon(1, 2, 1, 4))
+polygons[3].position = vec.Vector(2,2,0)
+polygons[3].velocity = vec.Vector(20,20,0)
+
+polygons.append(pol.Polygon(1, 2, 1, 7))
+polygons[4].position = vec.Vector(1,2,0)
+polygons[4].velocity = vec.Vector(20,20,0)
 
 #figure settings
 fig = pyplot.figure()
@@ -25,14 +33,15 @@ fig.set_dpi(100)
 fig.set_size_inches(7, 6.5)
 ax = pyplot.axes(xlim=(0, 10), ylim=(0, 10))
 
+#handles Vector vertices to suitable form for matplotlib
 def handleVertices(vertices):
     handled = []
     for i in range(0, len(vertices)):
         handled.append([vertices[i].x, vertices[i].y])
     return handled
 
+#create patches acording polygons
 patches = []
-
 for i in range(len(polygons)):
     patches.append(pyplot.Polygon(handleVertices(polygons[i].getVertices())))
 
@@ -46,29 +55,34 @@ def init():
     return []
 
 def animate(i, contact, patches):
-    for pol in polygons:
-        pol.update(20 / 1000, 9.81)
+    collisions = []
 
-        if pol.collidesWithBorder(0, 10, 0, 10):
-            pol.onBorderCollision(0, 10, 0, 10)
-        
+    #handle updating polygons and handling collisions
+    for polygon in polygons:
+        polygon.update(20 / 1000, 9.81)
+
+        if polygon.collidesWithBorder(0, 10, 0, 10):
+            polygon.onBorderCollision(0, 10, 0, 10)
+
         for other in polygons:
-            if other is not pol:
-                if pol.collides(other) and other.collides(pol):
-                    collision = pol.onCollision(other)
+            if other is not polygon:
+                if polygon.collides(other) and other.collides(polygon):
+                    #we dont want to process the same collision again
+                    alreadyCollided = False 
+                    for i in range(len(collisions)):
+                        if collisions[i][0] is other and collisions[i][1] is polygon:
+                            alreadyCollided = True
+                    if alreadyCollided:
+                        continue
+
+                    collisions.append((polygon, other))
+                    collision = polygon.onCollision(other)
                     contact.center = (collision.x, collision.y)
 
+    #update all the patches
     for i in range(len(patches)):
         patches[i].set_xy(handleVertices(polygons[i].getVertices()))
     return []
 
-
-anim = animation.FuncAnimation(fig, animate, 
-                               init_func=init,
-                               frames=1,
-                               fargs=(contact, patches,),
-                               interval=20,
-                               blit=True,
-                               repeat=True)
-
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=30, fargs=(contact, patches,), interval=1)
 pyplot.show()
